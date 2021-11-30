87777e2b42692b0ec0c299b2f4ceed07ec79c949
Cconkers <rubenpv011992@gmail.com>
2021-11-30 09:27:28 +0100

    pipesApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+ valueString: string = 'rUbEn';
+ valueNumber: number = 1000;
+ valueObject: object = {
+   name: 'Rubén'
+ };
+
+ mostrarNombre() {
+   console.log(this.valueString);
+ }
 }
